788151b347d56f7943e640b01433411c36652cd1
Joshua Lok <106495228+Jlok17@users.noreply.github.com>
2023-02-09 10:43:55 -0500

    Add files via upload

---
+++ SQL Homework #2 607.sql
@@ -0,0 +1,21 @@
+Create DATABASE SQL_Tutorial
+Create Table MovieRanking(
+Names VARCHAR(10) NULL,
+DarkKnight INT NULL,
+Interstellar VARCHAR (10) NULL,
+Gravity VARCHAR (10) NULL,
+KingKong VARCHAR (10) NULL,
+Cars3 VARCHAR (10) NULL,
+);
+
+
+INSERT INTO MovieRanking(Names, DarkKnight,Interstellar,Gravity,KingKong,Cars3)
+VALUES ('Josh',5,5,4,1,2),
+		('Nick',5,1,NULL,1,4),
+		('Max',5,1,3,1,NULL),
+		('Drew',3,2,4,1,NULL),
+		('Kyle',2,2,NULL,1,5),
+		('Viven',3,5,4,NULL,3),
+		('Brian',2,3,NULL,3,4),
+
+);
