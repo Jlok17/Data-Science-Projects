fa0e5d2df54894ee8fe4fe0b9fe7df4ea7ed94ec
Joshua Lok <106495228+Jlok17@users.noreply.github.com>
2023-02-23 13:51:24 -0500

    Add files via upload

---
+++ SQL Homework #5 607.sql
@@ -0,0 +1,19 @@
+USE SQL_Tutorial
+Create Table Homework5_607(
+Names VARCHAR(10) NULL,
+Flight_Status VARCHAR(10) NULL,
+Los_Angeles INT NULL,
+Pheonix INT NULL,
+San_Diego INT NULL,
+San_Francisco INT NULL,
+Seattle INT NULL,
+);
+
+
+INSERT INTO Homework5_607(Names, Flight_Status,Los_Angeles,Pheonix,San_Diego,San_Francisco,Seattle)
+VALUES ('Alaska','on_time',497,221,212,503,1841),
+		('Alaska','delayed',62,12,20,102,305),
+		('AMWEST','on_time',694,4840,383,320,201),
+		('AMWEST','delayed',117,415,65,129,61)
+		
+
